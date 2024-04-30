1602e13638f0af1f7a3addd8c9c0a5cef0f61548
Leo Gómez <leo.gomez@cttexpress.com>
2024-04-30 19:59:02 +0200

    fix(terraform): Adding new profile to aws provider

--- terraform/dev-cloud-app/provider.tf
+++ terraform/dev-cloud-app/provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region                   = "eu-central-1"
   shared_credentials_files = ["~/.aws/credentials"]
   shared_config_files      = ["~/.aws/config"]
-  profile                  = "default"
+  profile                  = "leogomez"
 }
