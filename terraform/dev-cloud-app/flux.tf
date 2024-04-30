49749ecb9f1775742f3b8720677dd4daab665d8c
Leo Gómez <leo.gomez@cttexpress.com>
2024-04-30 22:17:10 +0200

    fix(flux): TODO I need to terraform flux provider

--- terraform/dev-cloud-app/flux.tf
+++
@@ -1,7 +0,0 @@
-data "http" "flux_install" {
-  url = "https://github.com/fluxcd/flux2/releases/latest/download/install.yaml"
-}
-
-resource "kubernetes_manifest" "flux_install" {
-  manifest = yamldecode(data.http.flux_install.body)
-}
