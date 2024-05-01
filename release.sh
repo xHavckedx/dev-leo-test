b221a5642149f3fc88e61bb8a32567e51b198732
Leo Gómez <leo.gomez@cttexpress.com>
2024-05-01 18:30:38 +0200

    fix(releases.sh): bad formating

--- release.sh
+++ release.sh
@@ -5,23 +5,16 @@ last_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 if git rev-parse $last_tag >/dev/null 2>&1
 then
     echo "Tag $last_tag encontrado"
+    echo "El tag $last_tag sigue el versionado semántico"
 
-    # Verifica si el tag sigue el versionado semántico
-    if [[ $last_tag =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+    # Verifica si ya existe una rama release con el nombre del tag
+    if ! git rev-parse --verify release/$last_tag >/dev/null 2>&1
     then
-        echo "El tag $last_tag sigue el versionado semántico"
-
-        # Verifica si ya existe una rama release con el nombre del tag
-        if ! git rev-parse --verify release/$last_tag >/dev/null 2>&1
-        then
-            # Si la rama no existe, crea una nueva rama release con el nombre del tag
-            git checkout -b release/$last_tag
-            git push origin release/$last_tag
-        else
-            echo "La rama release/$last_tag ya existe"
-        fi
+        # Si la rama no existe, crea una nueva rama release con el nombre del tag
+        git checkout -b release/$last_tag
+        git push origin release/$last_tag
     else
-        echo "El tag $last_tag no sigue el versionado semántico"
+        echo "La rama release/$last_tag ya existe"
     fi
 else
     echo "No se encontró el tag $last_tag"
